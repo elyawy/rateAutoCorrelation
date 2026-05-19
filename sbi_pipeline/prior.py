07d338b8525a1da843b71d1c79ea9e8e377c8731
elyawy <elya.wygoda@gmail.com>
2026-05-19 12:03:07 +0300

    Refactor project structure: remove old files and add new feature extraction and training scripts

---
+++ sbi_pipeline/prior.py
@@ -0,0 +1,5 @@
+
+# Parameter ranges for random sampling
+ALPHA_RANGE = (0.01, 5.0)       # Min and Max Alpha
+RHO_RANGE = (0.01, 0.99)       # Min and Max Rho
+TREE_SCALE_RANGE = (0.01, 0.2) #  random_scale = 10 ** random.uniform(math.log10(SCALE_MIN), math.log10(SCALE_MAX))
