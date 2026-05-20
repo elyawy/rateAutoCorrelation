184efe6fabb583007a3571f925a4b75a41bf2481
elyawy <elya.wygoda@gmail.com>
2026-05-20 15:35:10 +0300

    .

--- sbi_pipeline/prior.py
+++ sbi_pipeline/prior.py
@@ -2,4 +2,4 @@
 # Parameter ranges for random sampling
 ALPHA_RANGE = (0.01, 2.0)       # Min and Max Alpha
 RHO_RANGE = (0.01, 0.99)       # Min and Max Rho
-TREE_SCALE_RANGE = (0.001, 0.1) #  random_scale = 10 ** random.uniform(math.log10(SCALE_MIN), math.log10(SCALE_MAX))
+TREE_SCALE_RANGE = (0.005, 0.1) #  random_scale = 10 ** random.uniform(math.log10(SCALE_MIN), math.log10(SCALE_MAX))
